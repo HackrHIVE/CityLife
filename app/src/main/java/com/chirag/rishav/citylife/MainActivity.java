ad48e76c6d6cc15e70094f3a825cfadebdd043c8
rishav7033 <37480850+rishav7033@users.noreply.github.com>
2018-11-14 21:50:03 +0530

    fragment_home button

--- app/src/main/java/com/chirag/rishav/citylife/MainActivity.java
+++ app/src/main/java/com/chirag/rishav/citylife/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -20,6 +22,8 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.ProviderQueryResult;
 import com.google.firebase.auth.SignInMethodQueryResult;
 
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
+
         mAuth = FirebaseAuth.getInstance();
         mainToolbar = findViewById( R.id.MainActivityToolbar );
         BottomNavigationView botnav = findViewById(R.id.botom_nav);
