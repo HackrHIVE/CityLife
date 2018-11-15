c416a93a686c31377466f1f8623767fe7597247e
rishav7033 <37480850+rishav7033@users.noreply.github.com>
2018-11-15 13:26:10 +0530

    Places to visit

---
+++ app/src/main/java/com/chirag/rishav/citylife/PlacesToVisit.java
@@ -0,0 +1,13 @@
+package com.chirag.rishav.citylife;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlacesToVisit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_places_to_visit);
+    }
+}
