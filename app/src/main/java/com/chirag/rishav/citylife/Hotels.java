80235870be79bfc7784d107f9d3d3f8226c4f03a
rishav7033 <37480850+rishav7033@users.noreply.github.com>
2018-11-15 10:52:21 +0530

    images for hotels and about city

---
+++ app/src/main/java/com/chirag/rishav/citylife/Hotels.java
@@ -0,0 +1,20 @@
+package com.chirag.rishav.citylife;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class Hotels extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hotels);
+
+
+    }
+}
---
+++ app/src/main/java/com/chirag/rishav/citylife/HowToReach.java
@@ -0,0 +1,18 @@
+package com.chirag.rishav.citylife;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HowToReach extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_how_to_reach);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+}
